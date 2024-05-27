40d9473f520d717a4f67bd5c08a47db3c3973d24
Jxck <jxck@jxck.io>
2024-05-27 16:44:15 +0900

    [layout] use Viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "mozaic.fm",
   description: "next generation web podcast",
-  viewport: "width=device-width, initial-scale=1.0",
+};
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
